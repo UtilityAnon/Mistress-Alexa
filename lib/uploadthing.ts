fe878b59222bb21601ed47f0138996f0cf404054
UtilityAnon <utilityanon100@yahoo.com>
2025-06-05 23:33:42 -0500

    Delete lib directory

--- lib/uploadthing.ts
+++
@@ -1,4 +0,0 @@
-import { createUploadthing, type FileRouter } from "uploadthing/client";
-import type { UploadRouter } from "../app/api/uploadthing/route";
-
-export const uploadthing = createUploadthing<UploadRouter>();
